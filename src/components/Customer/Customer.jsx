e7e6cc3bbd263e34f4410b7ae67ca77f556a8bc7
daddyloyal <lifeofemmany6@gmail.com>
2023-01-26 14:18:18 +0100

    Done with the swiper, moving on to the products component

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -2,6 +2,34 @@ import styles from "../style";
 import pave from "../../assets/pave.png";
 import vermaFarms from "../../assets/verma.png";
 import loveCircular from "../../assets/love-circular.png";
+import CaseSlider from "../Swiper/Swiper";
+
+const CASE_STUDIES = [
+  {
+    title: "Watch Sho's Story",
+    body: " Pave used Pipe side-by-side with equity to preserve ownership while they grew",
+    footer: "Pipe for SaaS",
+    image: pave,
+    overlay:"PAVE",
+  },
+  {
+    title: "Watch AJ's Story",
+    body: "Verma Farms got the funding they needed to complete an acquisition in less than 72 hours",
+    footer: "Pipe for D2C",
+    image: vermaFarms,
+    overlay:"VERMAFARMS",
+
+  },
+  {
+    title: "Watch Zaires's Story",
+    body: " Love Circular 2x'd their revenue and grew their team after each Pipe trade",
+    footer: "Pipe for Services",
+    image: loveCircular,
+    overlay:"Love Circular",
+
+  },
+]
+
 
 const Customer = () => {
   return (
@@ -22,53 +50,8 @@ const Customer = () => {
           Customer Stories
         </button>
       </div>
-      <div className="swiper flex">
-        {/* Card 1 */}
-        <div class="max-w-sm rounded-2xl overflow-hidden shadow-lg mx-auto my-8 border border-gray-800 bg-secondary hover:bg-card cursor-pointer">
-          <img class="w-full" src={pave} alt="pave" />
-          <div class="px-6 py-4">
-            <div class="font-bold font-inter text-md mb-2 text-white border-b border-slate-700 py-2">Watch Sho's Story</div>
-            <p class="text-tertiary font-normal font-inter text-xl">
-              Pave used Pipe side-by-side with equity to preserve ownership
-              while they grew
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block text-sm font-semibold text-tertiary mr-2">
-              Pipe for SaaS
-            </span>
-          </div>
-        </div>
-        {/* Card 2 */}
-        <div class="max-w-sm rounded-2xl overflow-hidden shadow-lg mx-auto my-8 border border-gray-800 bg-secondary hover:bg-card cursor-pointer">
-          <img class="w-full" src={vermaFarms} alt="verma" />
-          <div class="px-6 py-4">
-            <div class="font-bold font-inter text-md mb-2 text-white border-b border-slate-700 py-2">Watch AJ's Story</div>
-            <p class="text-tertiary font-normal font-inter text-xl">
-                Verma Farms got the funding they needed to complete an acquisition in less than 72 hours
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block text-sm font-semibold text-tertiary mr-2">
-                Pipe for D2C
-            </span>
-          </div>
-        </div>
-        {/* Card 3 */}
-        <div class="max-w-sm rounded-2xl overflow-hidden shadow-lg mx-auto my-8 border border-gray-800 bg-secondary hover:bg-card cursor-pointer">
-          <img class="w-full" src={loveCircular} alt="love-circular" />
-          <div class="px-6 py-4">
-            <div class="font-bold font-inter text-md mb-2 text-white border-b border-slate-700 py-2">Watch Zaires's Story</div>
-            <p class="text-tertiary font-normal font-inter text-xl">
-                Love Circular 2x’d their revenue and grew their team after each Pipe trade
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block text-sm font-semibold text-tertiary mr-2">
-              Pipe for Services
-            </span>
-          </div>
-        </div>
+      <div className="swiper">
+        <CaseSlider caseStudies={CASE_STUDIES} />
       </div>
     </section>
   );
--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,30 +1,67 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-export default () => {
+function CaseSlider({ caseStudies }) {
   return (
     <Swiper
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
+      breakpoints={{
+        50: {
+          slidesPerView: 1
+        },
+        1000: {
+          slidesPerView: 2
+        },
+        1200: {
+          slidesPerView: 3
+        }
+      }}
+      loop
+      autoplay
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {caseStudies.map((item, index) => (
+        <SwiperSlide key={index}>
+          <div class="max-w-sm rounded-2xl overflow-hidden group shadow-lg mx-auto my-8 border border-gray-800 bg-secondary hover:bg-card cursor-pointer">
+            <div className="relative h-[200px] overflow-y-hidden">
+              <img
+                class="w-full absolute inset-0"
+                src={item.image}
+                alt="pave"
+              />
+              <div className="bg-slate-700 translate-y-0 group-hover:translate-y-[-10rem] duration-700 grid place-items-center bg-opacity-50 transition w-full h-full relative opacity-100 group-hover:opacity-0">
+                <h2 className="font-bold text-white text-md font-inter">{item.overlay}</h2>
+              </div>
+            </div>
+            <div class="px-6 py-4">
+              <div class="font-bold font-inter text-md mb-2 text-white border-b border-slate-700 py-2">
+                {item.title}
+              </div>
+              <p class="text-tertiary hover:text-white font-normal font-inter text-xl">
+                {item.body}
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block text-xs font-inter font-semibold text-tertiary mr-2">
+                {item.footer}
+              </span>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
-};
+}
+
+export default CaseSlider;
